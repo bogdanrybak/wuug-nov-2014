e2bd874728dc8e0471dd9ab0cd75fcc1201f3a2b
Bogdan Rybak <boguzs@gmail.com>
2014-11-28 11:37:41 -0600

    Extra event interface implementation example

---
+++ Assets/2 - Main Components/ExampleOnHoverInterface.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+namespace Part2
+{
+    // Here's a list of all the interfaces that can be implemented
+    // https://bitbucket.org/Unity-Technologies/ui/src/ee188cc9dc2659e5404a912dd1344ade0855e6d0/UnityEngine.UI/EventSystem/EventInterfaces.cs?at=4.6
+    public class ExampleOnHoverInterface : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+    {
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            Debug.Log("Hovered");
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            Debug.Log("Clicked");
+        }
+    }
+}
